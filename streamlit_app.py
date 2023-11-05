de8a19bff8c4042986abfc993cdf430ad64c961f
jy-github301 <62967570+jy-github301@users.noreply.github.com>
2023-11-05 01:53:52 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 from langchain.chat_models import ChatOpenAI
+import pinecone
+from langchain.vectorstores import Pinecone
 from langchain.chains import ConversationChain
+from langchain.embeddings import SentenceTransformerEmbeddings
+
 from langchain.chains.conversation.memory import ConversationBufferWindowMemory
 from langchain.prompts import (
     SystemMessagePromptTemplate,
@@ -7,9 +11,31 @@ from langchain.prompts import (
     ChatPromptTemplate,
     MessagesPlaceholder
 )
+
 import streamlit as st
 from streamlit_chat import message
 from utils import *
+
+embeddings = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+
+
+
+pinecone.init(
+    api_key="",  # find at app.pinecone.io
+    environment="us-east-1-aws"  # next to api key in console
+)
+index_name = "langchain-chatbot"
+index = Pinecone.from_documents(docs, embeddings, index_name=index_name)
+
+
+def get_similiar_docs(query,k=1,score=False):
+  if score:
+    similar_docs = index.similarity_search_with_score(query,k=k)
+  else:
+    similar_docs = index.similarity_search(query,k=k)
+  return similar_docs
+
+
 
 st.subheader("Chatbot with Langchain, ChatGPT, Pinecone, and Streamlit")
 

e3d2856ff8037c25c9c0d9b5cbcbe9ff53d039b3
jy-github301 <62967570+jy-github301@users.noreply.github.com>
2023-11-05 01:24:29 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,68 +1,38 @@
 import streamlit as st
-import openai
-from langchain.embeddings.huggingface import HuggingFaceEmbeddings
 import pinecone
-from langchain.vectorstores import Pinecone
-from langchain.llms import OpenAI
-import os
-from langchain.chat_models import ChatOpenAI
-from langchain.chains import ConversationalRetrievalChain
 
+pinecone_key="8ddf1bc3-23b1-41d1-89a7-4052bf264f7c"
+pinecone_environment='gcp-starter'
+index_name = 'tax-rag'
 
-embed_model = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')
-
+# Set up Pinecone
 
 pinecone.init(api_key=pinecone_key, environment=pinecone_environment)
-index = pinecone.Index('tax-rag')
-vectorstore = Pinecone(index, embed_model.embed_query, text_field = "text")
+pinecone_index = pinecone.Index('tax-rag')
 
+# Create a Streamlit widget for inputting vectors
+input_vector = st.text_input("Enter a vector (comma-separated):")
 
+# Convert the input to a list of floats
+query_vector = [float(x.strip()) for x in input_vector.split(",")]
 
-st.set_page_config(page_title="TaxPro", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
-st.title("TaxPro")
+# Query Pinecone for similar vectors
+if query_vector:
+    results = pinecone_index.query(queries=[query_vector], top_k=10)
+    for result in results:
+        st.write(result)
 
-# Sidebar for entering OpenAI key
-with st.sidebar:
-    st.title('OpenAI key')
-    openai_key = st.text_input('Enter OpenAI key:', type='password')
-    if not openai_key:
-        st.warning('Please enter your OpenAI key!', icon='⚠️')
-    else:
-        st.success('Proceed to entering your prompt message!', icon='👉')
+# Add a button for adding vectors to Pinecone
+add_button = st.button("Add vector")
+if add_button:
+    vector_input = st.text_input("Enter a vector to add (comma-separated):")
+    vector = [float(x.strip()) for x in vector_input.split(",")]
+    id_input = st.text_input("Enter an ID for the vector:")
+    pinecone_index.add(vectors=[vector], ids=[id_input])
 
- # Store chat messages, and initialize the chat message history
-
-if "messages" not in st.session_state.keys():
-    st.session_state.messages = [{"role": "assistant", "content": "Ask me any questions"}]
-
-# Display the prior chat messages
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.write(message["content"]) 
-
-# User-provided prompt
-if prompt := st.chat_input(disabled=not openai_key):
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    with st.chat_message("user"):
-        st.write(prompt)
-
-# Function to get the GPT3.5's response
-def get_assistant_response(allmessages):
-
-    llm=ChatOpenAI(temperature=0.3, model_name="gpt-3.5-turbo",openai_api_key=OPENAI_API_KEY)
-
-    chain = ConversationalRetrievalChain.from_llm(llm,
-                                              vectorstore.as_retriever(search_kwargs={'k': 5}),
-                                              return_source_documents=True)
-         
-    response = chain.result.message.content
-    return response
-
-if len(st.session_state.messages)>1:
-    with st.chat_message("assistant"):
-        with st.spinner("Thinking..."):
-            response = get_assistant_response(st.session_state.messages) 
-            st.write(response)
-
-    newmessage = {"role": "assistant", "content": response}
-    st.session_state.messages.append(newmessage)
+# Add a slider for controlling the number of results to display
+num_results = st.slider("Number of results to display", min_value=1, max_value=100, value=10)
+if query_vector:
+    results = pinecone_index.query(queries=[query_vector], top_k=num_results)
+    for result in results:
+        st.write(result)

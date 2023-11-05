92e42784eccb5982efef3475e4cbbce14a2e339c
jy-github301 <62967570+jy-github301@users.noreply.github.com>
2023-11-05 00:59:27 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,45 +1,68 @@
 import streamlit as st
-from llama_index import VectorStoreIndex, ServiceContext, Document
-from llama_index.llms import OpenAI
 import openai
-from llama_index import SimpleDirectoryReader
+from langchain.embeddings.huggingface import HuggingFaceEmbeddings
+import pinecone
+from langchain.vectorstores import Pinecone
+from langchain.llms import OpenAI
+import os
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import PromptTemplate
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
 
-st.set_page_config(page_title="Chat with the Streamlit docs, powered by LlamaIndex", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
-openai.api_key = st.secrets.openai_key
-st.title("Chat with the Streamlit docs, powered by LlamaIndex 💬🦙")
-st.info("Check out the full tutorial to build this app in our [blog post](https://blog.streamlit.io/build-a-chatbot-with-custom-data-sources-powered-by-llamaindex/)", icon="📃")
-         
-if "messages" not in st.session_state.keys(): # Initialize the chat messages history
-    st.session_state.messages = [
-        {"role": "assistant", "content": "Ask me a question about Streamlit's open-source Python library!"}
-    ]
 
-@st.cache_resource(show_spinner=False)
-def load_data():
-    with st.spinner(text="Loading and indexing the Streamlit docs – hang tight! This should take 1-2 minutes."):
-        reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
-        docs = reader.load_data()
-        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="You are an expert on the Streamlit Python library and your job is to answer technical questions. Assume that all questions are related to the Streamlit Python library. Keep your answers technical and based on facts – do not hallucinate features."))
-        index = VectorStoreIndex.from_documents(docs, service_context=service_context)
-        return index
+embed_model = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')
 
-index = load_data()
 
-if "chat_engine" not in st.session_state.keys(): # Initialize the chat engine
-        st.session_state.chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
+pinecone.init(api_key=pinecone_key, environment=pinecone_environment)
+index = pinecone.Index('tax-rag')
+vectorstore = Pinecone(index, embed_model.embed_query, text_field = "text")
 
-if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
-    st.session_state.messages.append({"role": "user", "content": prompt})
 
-for message in st.session_state.messages: # Display the prior chat messages
+
+st.set_page_config(page_title="TaxPro", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
+st.title("TaxPro")
+
+# Sidebar for entering OpenAI key
+with st.sidebar:
+    st.title('OpenAI key')
+    openai_key = st.text_input('Enter OpenAI key:', type='password')
+    if not openai_key:
+        st.warning('Please enter your OpenAI key!', icon='⚠️')
+    else:
+        st.success('Proceed to entering your prompt message!', icon='👉')
+
+ # Store chat messages, and initialize the chat message history
+
+if "messages" not in st.session_state.keys():
+    st.session_state.messages = [{"role": "assistant", "content": "Ask me any questions"}]
+
+# Display the prior chat messages
+for message in st.session_state.messages:
     with st.chat_message(message["role"]):
-        st.write(message["content"])
+        st.write(message["content"]) 
 
-# If last message is not from assistant, generate a new response
-if st.session_state.messages[-1]["role"] != "assistant":
+# User-provided prompt
+if prompt := st.chat_input(disabled=not openai_key):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.write(prompt)
+
+# Function to get the GPT3.5's response
+def get_assistant_response(allmessages):
+
+    chain = ConversationalRetrievalChain.from_llm(llm=ChatOpenAI(temperature=0.3, model_name="gpt-3.5-turbo",openai_api_key=OPENAI_API_KEY),
+                                              vectorstore.as_retriever(search_kwargs={'k': 5}),
+                                              return_source_documents=True)
+         
+    response = chain.result.message.content
+    return response
+
+if len(st.session_state.messages)>1:
     with st.chat_message("assistant"):
         with st.spinner("Thinking..."):
-            response = st.session_state.chat_engine.chat(prompt)
-            st.write(response.response)
-            message = {"role": "assistant", "content": response.response}
-            st.session_state.messages.append(message) # Add response to message history
+            response = get_assistant_response(st.session_state.messages) 
+            st.write(response)
+
+    newmessage = {"role": "assistant", "content": response}
+    st.session_state.messages.append(newmessage)
